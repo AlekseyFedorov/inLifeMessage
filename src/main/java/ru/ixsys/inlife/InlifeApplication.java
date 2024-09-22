f7bcd380d2ed55daab36cac4122973b5d6cdad92
Aleksey Fedorov <Aleksey.G.Fedorov@gmail.com>
2024-09-22 10:16:22 +0300

    First initialization

---
+++ src/main/java/ru/ixsys/inlife/InlifeApplication.java
@@ -0,0 +1,13 @@
+package ru.ixsys.inlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InlifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InlifeApplication.class, args);
+    }
+
+}
