0850d29a604b7a0cb8f0344d41f362150d8dfad4
Aleksey Fedorov <Aleksey.G.Fedorov@gmail.com>
2024-09-23 08:00:59 +0300

    First initialization

--- src/main/java/ru/ixsys/inlife/controler/MessageController.java
+++ src/main/java/ru/ixsys/inlife/controler/MessageController.java
@@ -3,9 +3,19 @@ package ru.ixsys.inlife.controler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.ixsys.inlife.model.Message;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/Messages")
 public class MessageController {
 
+    public List<Message> findAllMessages() {
+        return List.of(
+                new Message("title 1", "comment 1"),
+                new Message("titlw 2", "comment 2"),
+                new Message("title 3", "comment 3")
+        );
+    }
 }
